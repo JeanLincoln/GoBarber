ea7d042e8b278e1d5ce26e613151de7f98cbd17d
Jean Lincoln <jeanlincol@gmail.com>
2022-10-07 18:02:03 -0300

    ScheduleList component, calendar added, schedules state added

--- src/components/ScheduleList/index.tsx
+++ src/components/ScheduleList/index.tsx
@@ -1,55 +1,156 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Clock } from 'phosphor-react'
 import { Header } from '../Header'
 import { Schedule } from '../Schedule'
+import { DayPicker } from 'react-day-picker'
+import { useState } from 'react'
+import pt from 'date-fns/locale/pt-BR'
+import 'react-day-picker/dist/style.css'
 import * as S from './styles'
+import { format } from 'date-fns'
+
+const weekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado'
+]
+
+const disabledDays = [
+  new Date('2022-10-01T00:00:00'),
+  new Date('2022-10-02T00:00:00'),
+  new Date('2022-10-08T00:00:00'),
+  new Date('2022-10-09T00:00:00')
+]
+
+type Schedule = {
+  scheduleDate: Date
+  userImage: string
+  userName: string
+}
+
+const Testschedules = [
+  {
+    scheduleDate: new Date('2022-10-06T08:00:00'),
+    userImage: '/images/10.jpg',
+    userName: 'Leonardo Minatti'
+  },
+  {
+    scheduleDate: new Date('2022-10-06T09:00:00'),
+    userImage: '/images/47.jpg',
+    userName: 'Sarah Souza'
+  },
+  {
+    scheduleDate: new Date('2022-10-06T11:00:00'),
+    userImage: '/images/52.jpg',
+    userName: 'Bruna Ferreira'
+  },
+  {
+    scheduleDate: new Date('2022-10-06T14:00:00'),
+    userImage: '/images/39.jpg',
+    userName: 'Cleiton Souza'
+  },
+  {
+    scheduleDate: new Date('2022-10-06T15:00:00'),
+    userImage: '/images/19.jpg',
+    userName: 'Rolangela B. Rusena'
+  }
+]
 
 export function ScheduleList() {
+  const [selectedDay, setSelectedDay] = useState<Date | undefined>(new Date())
+  const [schedules] = useState<Schedule[]>(Testschedules)
+
+  const selectedDaySchedules = () =>
+    schedules.filter(
+      (schedule) =>
+        selectedDay!.getDate() === Number(format(schedule.scheduleDate, 'd'))
+    )
+
   return (
     <>
       <Header />
-      <S.ScheculeListContainer>
-        <S.HeadingContainer>
-          <h1>times agendados</h1>
-          <span>Hoje | Dia 06 | Segunda-feira</span>
-        </S.HeadingContainer>
-        <span>Atendimento a seguir</span>
-        <S.NextScheduleContainer>
-          <div>
-            <img src="images/10.jpg" />
-            <span>Leonardo Minatti</span>
-          </div>
-          <div>
-            <Clock size={24} />
-            <span>08:00</span>
-          </div>
-        </S.NextScheduleContainer>
-        <S.DayPartScheduleContainer>
-          <S.DayPartText>Manhã</S.DayPartText>
-          <Schedule
-            time="08:00"
-            userImage="/images/39.jpg"
-            userName="Cleiton Souza"
-          />
-          <Schedule
-            time="11:00"
-            userImage="/images/19.jpg"
-            userName="Rolangela B. Rusena"
-          />
-        </S.DayPartScheduleContainer>
-        <S.DayPartScheduleContainer>
-          <S.DayPartText>Tarde</S.DayPartText>
-          <Schedule
-            time="13:00"
-            userImage="/images/47.jpg"
-            userName="Sarah Souza"
-          />
-          <Schedule
-            time="15:00"
-            userImage="/images/52.jpg"
-            userName="Bruna Ferreira"
-          />
-        </S.DayPartScheduleContainer>
-      </S.ScheculeListContainer>
+      <S.PageContainer>
+        {!selectedDaySchedules().length ? (
+          <S.ScheculeListContainer>
+            <h1>Nao tem agendamentos</h1>
+          </S.ScheculeListContainer>
+        ) : (
+          <S.ScheculeListContainer>
+            <S.HeadingContainer>
+              <h1>Horarios agendados</h1>
+              <span>
+                Dia{' '}
+                {selectedDay!.getDate() < 10
+                  ? `0${selectedDay!.getDate()}`
+                  : selectedDay!.getDate()}{' '}
+                | {weekDays[selectedDay!.getDay()]}
+              </span>
+            </S.HeadingContainer>
+            <span>Atendimento a seguir</span>
+            <S.NextScheduleContainer>
+              <div>
+                {<img src={selectedDaySchedules()[0].userImage} />}
+                <span>{selectedDaySchedules()[0].userName}</span>
+              </div>
+              <div>
+                <Clock size={24} />
+                <span>
+                  {format(selectedDaySchedules()[0].scheduleDate, 'p', {
+                    locale: pt
+                  })}
+                </span>
+              </div>
+            </S.NextScheduleContainer>
+            <S.DayPartScheduleContainer>
+              <S.DayPartText>Manhã</S.DayPartText>
+              {selectedDaySchedules()
+                .filter(
+                  (schedule) =>
+                    Number(format(schedule.scheduleDate, 'k')) < 12 &&
+                    schedules.indexOf(schedule) !== 0
+                )
+                .map(({ scheduleDate, userImage, userName }) => (
+                  <Schedule
+                    key={userName}
+                    time={format(scheduleDate, 'p', { locale: pt })}
+                    userImage={userImage}
+                    userName={userName}
+                  />
+                ))}
+            </S.DayPartScheduleContainer>
+            <S.DayPartScheduleContainer>
+              <S.DayPartText>Tarde</S.DayPartText>
+              {selectedDaySchedules()
+                .filter(
+                  (schedule) =>
+                    Number(format(schedule.scheduleDate, 'k')) > 12 &&
+                    schedules.indexOf(schedule) !== 0
+                )
+                .map(({ scheduleDate, userImage, userName }) => (
+                  <Schedule
+                    key={userName}
+                    time={format(scheduleDate, 'p', { locale: pt })}
+                    userImage={userImage}
+                    userName={userName}
+                  />
+                ))}
+            </S.DayPartScheduleContainer>
+          </S.ScheculeListContainer>
+        )}
+        <S.DayPickerContainer>
+          <DayPicker
+            locale={pt}
+            mode="single"
+            disabled={disabledDays}
+            selected={selectedDay}
+            onSelect={setSelectedDay}
+          ></DayPicker>
+        </S.DayPickerContainer>
+      </S.PageContainer>
     </>
   )
 }
--- src/components/ScheduleList/styles.ts
+++ src/components/ScheduleList/styles.ts
@@ -1,16 +1,89 @@
 import styled, { css } from 'styled-components'
 
+export const PageContainer = styled.div`
+  display: flex;
+  gap: 12rem;
+  margin-right: 16rem;
+`
 export const ScheculeListContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 16rem;
   margin-top: 6.4rem;
-  max-width: 64rem;
+  width: 64rem;
+  min-width: 45rem;
 
   span {
     font-size: 2rem;
   }
 `
+export const DayPickerContainer = styled.div`
+  ${({ theme }) => css`
+    width: 35.9rem;
+    height: 100%;
+
+    margin-top: 6.4rem;
+
+    .rdp-table {
+      border-bottom-left-radius: 10px;
+      border-bottom-right-radius: 10px;
+      width: 35.9rem;
+      background-color: ${theme['gray-800']};
+
+      .rdp-head {
+        span {
+          font-size: 1.4rem;
+        }
+      }
+
+      td {
+        button {
+          margin: 0.5rem;
+          font-size: 1.4rem;
+          background-color: ${theme['gray-600']};
+          border-radius: 10px;
+          border: none;
+          color: ${theme['white']};
+        }
+
+        .rdp-day_selected {
+          background-color: ${theme['orange-500']};
+          color: ${theme['gray-900']};
+        }
+      }
+    }
+
+    .rdp-caption {
+      height: 5rem;
+      padding: 0 1rem;
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      background-color: ${theme['gray-600']};
+    }
+
+    .rdp-nav {
+      display: flex;
+      gap: 1rem;
+
+      button {
+        cursor: pointer;
+        border: none;
+        background-color: transparent;
+        color: ${theme['white']};
+      }
+    }
+
+    h2 {
+      color: ${theme['white']};
+      font-weight: 700;
+      font-size: 1.6rem;
+    }
+  `}
+`
+
 export const HeadingContainer = styled.div`
   ${({ theme }) => css`
     display: flex;
